509245959e7b200a5216c22140a6ae1e284f2c97
sunxiao <xiao_sun@umail.ucsb.edu>
2017-11-10 11:48:31 -0800

    fixed indentations

--- src/edu/ucsb/cs56/projects/games/fish_animation/Bubbles.java
+++ src/edu/ucsb/cs56/projects/games/fish_animation/Bubbles.java
@@ -20,72 +20,72 @@ import java.awt.geom.AffineTransform;
 
 public class Bubbles extends ScumOfTheSea
 {
-    private double x, y;
-    private int diameter;
-    
-    /** 
+	private double x, y;
+	private int diameter;
+	
+	/** 
 	Bubbles Constructor
 	@param x sets the x position
 	@param y sets the y position
 	@param diameter sets the diameter of the Bubble
-    */
-    Bubbles(double x, double y, int diameter) {
+	*/
+	Bubbles(double x, double y, int diameter) {
 	this.x = x;
 	this.y = y;
 	this.diameter = diameter;
-    }
+	}
 
-    /** 
+	/** 
 	Gets the X position of the Bubble
 	@return X the position of the Bubble
-    */
-    @Override 
-    public double getXPos() {
+	*/
+	@Override 
+	public double getXPos() {
 	return x;
-    }
-    
-    /** 
+	}
+	
+	/** 
 	Gets the Y position of the Bubble
 	@return Y the position of the Bubble
-    */
-    @Override 
-    public double getYPos() {
+	*/
+	@Override 
+	public double getYPos() {
 	return y;
-    }
-    
-    /** 
+	}
+	
+	/** 
 	Gets the Diameter
 	@return Returns the diameter of the object
-    */
-    int getDiameter() {
+	*/
+	int getDiameter() {
 	return diameter;
-    }
-    
-    /** 
+	}
+	
+	/** 
 	Sets the X position of the Bubbles object
 	@param x sets the X position of the Bubbles ojbect
-    */
-    @Override 
-    public void setX(double x) {
+	*/
+	@Override 
+	public void setX(double x) {
 	this.x = x;
-    }
-    
-    /** 
+	}
+	
+	/** 
 	Sets the Y position of the Bubbles object
 	@param y sets the Y position of the Bubbles object
-    */
-    @Override 
-    public void setY(double y) {
+	*/
+	@Override 
+	public void setY(double y) {
 	this.y = y;
-    }
-    
-    /** 
+	}
+	
+	/** 
 	Changes the position of the Bubble
 	@param m changes the position of the Bubbles object
-    */
-    @Override 
-    public void move(double m) {
+	*/
+	@Override 
+	public void move(double m) {
 	y -= m;
-    }
+	}
 }
 	
