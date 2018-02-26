c95e848325fccd7167f0fe6633f22edc5c4c9397
Bryan Wu <bryanwu@umail.ucsb.edu>
2018-02-25 23:16:52 -0800

    BW/SH ArrayOutOfBounds fix attempt 7

--- src/edu/ucsb/cs56/projects/games/fish_animation/FishAnimationEnvironment.java
+++ src/edu/ucsb/cs56/projects/games/fish_animation/FishAnimationEnvironment.java
@@ -340,9 +340,9 @@ public class FishAnimationEnvironment extends JFrame implements Serializable {
 			    random = (int)(Math.random() * seaFloorSpriteFileNames.size());
                 seaFloorURLList.add(getClass().getResource("/resources/fish_sprites/" + seaFloorSpriteFileNames.get(random)));
             }
-            
+
 			// Draws the seaweed at the specified points
-			for (int i = 0; i < this.getWidth(); i += 64, index++) {
+			for (int i = 0; i < this.getWidth(); i += 64++) {
                 seaFloor = new ImageIcon(seaFloorURLList.get(i/64)).getImage();
                 if (i < 11)
 				    g.drawImage(seaFloor, i, this.getHeight() - 64, this);
